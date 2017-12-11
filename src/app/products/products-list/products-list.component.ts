d0659694a0a5c84bd278509483422685c77ebf84
马博 <dtmbo@hotmail.com>
2017-12-10 22:01:02 -0600

    first commit

---
+++ src/app/products/products-list/products-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../Product';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styles: [`
+    ul{
+      border: 1px solid green;
+      margin: 20px 30px;
+    }
+  `]
+})
+export class ProductsListComponent implements OnInit {
+
+  products: Product[] = [
+    new Product(1, 'iPhone', 300,'This is a phone'),
+    new Product(2, 'iPad', 400,'This is a ipad'),
+    new Product(3, 'Apple TV', 1000,'This is a TV'),
+  ]
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
